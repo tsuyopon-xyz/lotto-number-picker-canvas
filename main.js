3f6e05c3958b44f75cf920dee7e025b840234825
Cheekyfunkymonkey <shigechu@gmail.com>
2019-04-03 11:40:49 +1300

    Fix various things

--- main.js
+++ main.js
@@ -2,24 +2,18 @@
 
 (() => {
   // Set how many numbers to use.
-  const no_of_num = 40;
-
-  // Set y-axis of each numbers on the image.
-  const num_y_axis = [];
+  const NO_OF_NUM = 40;
 
   // This boolean decides weather the app is stopped or not.
   let isPaused = false;
 
   // Set how fast the image changes. Smaller number means faster.
-  const speed = [30, 10, 100, 1000, 20, 200];
+  const SPEED = [30, 40, 10, 5, 200, 5];
+
+  const ONE_SECOND = 1000;
 
   // Set the image paths.
-  const image_path1 = "images/numbers1.png";
-  const image_path2 = "images/numbers2.png";
-  const image_path3 = "images/numbers3.png";
-  const image_path4 = "images/numbers4.png";
-  const image_path5 = "images/numbers5.png";
-  const image_path6 = "images/numbers6.png";
+  const IMAGE_PATH = "images/numbers1.png";
 
   // Set the current indexes.
   let currentIndex1 = 0;
@@ -29,18 +23,14 @@
   let currentIndex5 = 0;
   let currentIndex6 = 0;
 
-  // Create array for y-axis of the image.
-  function createArray() {
-    let y = 40;
-    for (let i = 0; i < no_of_num; i++) {
-      if (num_y_axis.length) {
-        y += 94.5;
-        num_y_axis.push(y);
-      } else {
-        num_y_axis.push(y);
-      }
-    }
-  }
+  // Set index1 to index6 for when "stop" button pressed
+  let index1 = 0;
+  let index2 = 0;
+  let index3 = 0;
+  let index4 = 0;
+  let index5 = 0;
+  let index6 = 0;
+
   // Get element by id.
   const numberCanvas1 = document.getElementById("number1");
   const numberCanvas2 = document.getElementById("number2");
@@ -60,20 +50,10 @@
   const numberContext6 = numberCanvas6.getContext("2d");
 
   // Create new Image objects.
-  const imageObj1 = new Image();
-  const imageObj2 = new Image();
-  const imageObj3 = new Image();
-  const imageObj4 = new Image();
-  const imageObj5 = new Image();
-  const imageObj6 = new Image();
+  const imageObj = new Image();
 
   // Set src for image objects.
-  imageObj1.src = image_path1;
-  imageObj2.src = image_path2;
-  imageObj3.src = image_path3;
-  imageObj4.src = image_path4;
-  imageObj5.src = image_path5;
-  imageObj6.src = image_path6;
+  imageObj.src = IMAGE_PATH;
 
   // Main function for this app.
   function main() {
@@ -88,93 +68,84 @@
       window.location.reload();
     });
 
-    imageObj1.onload = () => {
-      function loop1() {
-        if (!isPaused) {
-          draw(numberCanvas1, numberContext1, imageObj1, currentIndex1);
-          setTimeout(loop1, 1000 / speed[0]);
-          if (currentIndex1 < 40) {
-            currentIndex1++;
-          } else {
-            currentIndex1 = 0;
-          }
-        }
-      }
-      function loop2() {
-        if (!isPaused) {
-          draw(numberCanvas2, numberContext2, imageObj2, currentIndex2);
-          setTimeout(loop2, 1000 / speed[1]);
-          if (currentIndex2 < 40) {
-            // currentIndex = 10;
-            currentIndex2++;
-          } else {
-            currentIndex2 = 0;
-          }
-        }
-      }
-      function loop3() {
-        if (!isPaused) {
-          draw(numberCanvas3, numberContext3, imageObj3, currentIndex3);
-          setTimeout(loop3, 1000 / speed[2]);
-          if (currentIndex3 < 40) {
-            currentIndex3++;
-          } else {
-            currentIndex3 = 0;
-          }
-        }
-      }
-      function loop4() {
-        if (!isPaused) {
-          draw(numberCanvas4, numberContext4, imageObj4, currentIndex4);
-          setTimeout(loop4, 1000 / speed[3]);
-          if (currentIndex4 < 40) {
-            currentIndex4++;
-          } else {
-            currentIndex4 = 0;
-          }
-        }
-      }
-      function loop5() {
-        if (!isPaused) {
-          draw(numberCanvas5, numberContext5, imageObj5, currentIndex5);
-          setTimeout(loop5, 1000 / speed[4]);
-          if (currentIndex5 < 40) {
-            currentIndex5++;
-          } else {
-            currentIndex5 = 0;
-          }
-        }
-      }
-      function loop6() {
-        if (!isPaused) {
-          draw(numberCanvas6, numberContext6, imageObj6, currentIndex6);
-          setTimeout(loop6, 1000 / speed[5]);
-          if (currentIndex6 < 40) {
-            currentIndex6++;
-          } else {
-            currentIndex6 = 0;
-          }
-        }
-      }
-      stopButton.addEventListener("click", () => {
-        isPaused = true;
-      });
+    imageObj.onload = () => {
+      draw(numberCanvas1, numberContext1, imageObj, currentIndex1, SPEED[0]);
+      draw(numberCanvas2, numberContext2, imageObj, currentIndex2, SPEED[1]);
+      draw(numberCanvas3, numberContext3, imageObj, currentIndex3, SPEED[2]);
+      draw(numberCanvas4, numberContext4, imageObj, currentIndex4, SPEED[3]);
+      draw(numberCanvas5, numberContext5, imageObj, currentIndex5, SPEED[4]);
+      draw(numberCanvas6, numberContext6, imageObj, currentIndex6, SPEED[5]);
 
-      createArray();
-      loop1();
-      loop2();
-      loop3();
-      loop4();
-      loop5();
-      loop6();
+      stopButton.addEventListener("click", () => {
+        // Set array for each index when "stop" button pressed.
+        const indexes = [index1, index2, index3, index4, index5, index6];
+
+        // Check for duplicated numbers.
+        const duplicatedIndexes = indexes.filter((x, i, self) => {
+          return self.indexOf(x) !== i;
+        });
+        console.log(duplicatedIndexes);
+        if (duplicatedIndexes.length === 0) {
+          isPaused = true;
+        } else {
+          fixDuplication(indexes, duplicatedIndexes);
+          isPaused = true;
+        }
+      });
     };
   }
 
-  // Function for drawing the image on a canvas.
-  function draw(canvas, context, imageObject, index) {
-    const sx = 20;
-    const sy = num_y_axis[index];
+  // Function for drawing the image on each canvas repeatedly.
+  function draw(canvas, context, imageObject, currentIndex, speed) {
+    function loop() {
+      if (!isPaused) {
+        const sx = 20;
+        const sy = 40 + 94.5 * currentIndex; // 40 is where the first number of y-axis is and if you add 94.5 the image will jump to the next number.
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
+        context.drawImage(
+          imageObject,
+          sx,
+          sy,
+          80,
+          80,
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        );
+
+        setTimeout(loop, ONE_SECOND / speed);
+
+        if (currentIndex < NO_OF_NUM - 1) {
+          currentIndex++;
+        } else {
+          currentIndex = 0;
+        }
+
+        // Get each index when stop button is pressed.
+        if (canvas === numberCanvas1) {
+          index1 = currentIndex;
+        } else if (canvas === numberCanvas2) {
+          index2 = currentIndex;
+        } else if (canvas === numberCanvas3) {
+          index3 = currentIndex;
+        } else if (canvas === numberCanvas4) {
+          index4 = currentIndex;
+        } else if (canvas === numberCanvas5) {
+          index5 = currentIndex;
+        } else if (canvas === numberCanvas6) {
+          index6 = currentIndex;
+        }
+      }
+    }
+    loop();
+  }
+
+  // This is the function to draw image in the canvas where a duplicated number is.
+  function drawAgain(canvas, context, imageObject, index) {
+    const sx = 20;
+    const sy = 40 + 94.5 * index; // 40 is where the first number of y-axis is and if you add 94.5 the image will jump to the next number.
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     context.drawImage(
@@ -189,5 +160,36 @@
       canvas.height
     );
   }
+
+  // Change a duplicated number to an other random number.
+  function fixDuplication(indexes, duplicatedIndexes) {
+    // Find out the index of duplicated number in indexes array.
+    const num = indexes.indexOf(duplicatedIndexes[0]);
+
+    // Declare max number for Math.floor function and new index.
+    const max = NO_OF_NUM - 1;
+    let newIndex;
+
+    // Set a new index which does not duplicate with any other numbers on the canvases.
+    while (newIndex === undefined || indexes[newIndex] >= 0) {
+      newIndex = Math.floor(Math.random() * Math.floor(max));
+    }
+
+    // Draw image for the canvas where duplicated number is.
+    if (num === 0) {
+      drawAgain(numberCanvas1, numberContext1, imageObj, newIndex);
+    } else if (num === 1) {
+      drawAgain(numberCanvas2, numberContext2, imageObj, newIndex);
+    } else if (num === 2) {
+      drawAgain(numberCanvas3, numberContext3, imageObj, newIndex);
+    } else if (num === 3) {
+      drawAgain(numberCanvas4, numberContext4, imageObj, newIndex);
+    } else if (num === 4) {
+      drawAgain(numberCanvas5, numberContext5, imageObj, newIndex);
+    } else if (num === 5) {
+      drawAgain(numberCanvas6, numberContext6, imageObj, newIndex);
+    }
+  }
+
   main();
 })();
